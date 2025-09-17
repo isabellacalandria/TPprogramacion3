4b98be031612cb2b3165058134590234a1fbc0f2
fgarciaaltare <fgarciaaltare@udesa.edu.ar>
2025-09-17 16:37:46 -0300

    hicimos el detalle de ver mas y ver menos

--- src/components/CardSeries/CardSeries.js
+++ src/components/CardSeries/CardSeries.js
@@ -1,24 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import '../Series/Series.css'; // usa los mismos estilos que Series
+import "../Series/Series.css";
 
-function CardSeries({ serie }) {
-  return (
-    <article key={serie.id} className="single-card-movie">
-      <img
-        src={`https://image.tmdb.org/t/p/w342${serie.poster_path}`}
-        alt={serie.name}
-        className="card-img-top"
-      />
-      <div className="cardBody">
-        <h5 className="card-title">{serie.name}</h5>
-        <p className="card-text">{serie.overview}</p>
-        <Link to={`/series/${serie.id}`} className="btn btn-primary">
-        Ver detalle
-        </Link>
-      </div>
-    </article>
-  );
+class CardSeries extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      verMas: false,
+      textoBoton: "Ver Mas",
+      textoClassName: "infoTapada", // empieza oculta
+    };
+  }
+
+  cambiar() {
+    if (this.state.verMas === false) {
+      this.setState({
+        verMas: true,
+        textoBoton: "Ver Menos",
+        textoClassName: "",
+      });
+    } else {
+      this.setState({
+        verMas: false,
+        textoBoton: "Ver Mas",
+        textoClassName: "infoTapada",
+      });
+    }
+  }
+
+  render() {
+    const { serie } = this.props;
+
+    return (
+      <article key={serie.id} className="single-card-movie">
+        <img
+          src={`https://image.tmdb.org/t/p/w342${serie.poster_path}`}
+          alt={serie.name}
+          className="card-img-top"
+        />
+        <div className="cardBody">
+          <h5 className="card-title">{serie.name}</h5>
+
+          {/* Botón ver más / menos */}
+          <p className="more" onClick={() => this.cambiar()}>
+            {this.state.textoBoton}
+          </p>
+
+          {/* Descripción controlada por className */}
+          <p className={`card-text ${this.state.textoClassName}`}>
+            {serie.overview}
+          </p>
+
+          <Link to={`/series/${serie.id}`} className="btn btn-primary">
+            Ver detalle
+          </Link>
+        </div>
+      </article>
+    );
+  }
 }
 
 export default CardSeries;
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
+import CardPelis from "../../components/CardPelis/CardPelis";
+import CardSeries from "../../components/CardSeries/CardSeries";
 import { Link } from "react-router-dom";
 
 class Home extends Component {
@@ -13,11 +15,10 @@ class Home extends Component {
     this.state = {
       peliculasNowPlaying: [],
       seriesHoy: [],
-      peliculasPopulares: [], 
-      seriesPopulares: [], 
-      favoritos: guardados
+      peliculasPopulares: [],
+      seriesPopulares: [],
+      favoritos: guardados,
     };
-  
   }
 
   componentDidMount() {
@@ -30,21 +31,32 @@ class Home extends Component {
       },
     };
 
-    fetch("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1", options)
-      .then((res) => res.json())
-      .then((data) => { 
-      let cuatro= data.results.filter((peli,idx)=>idx<4)
-      this.setState({ peliculasNowPlaying: cuatro })})
-      .catch((err) => console.error(err));
-
-    fetch("https://api.themoviedb.org/3/tv/airing_today?language=en-US&page=1", options)
+    fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      options
+    )
       .then((res) => res.json())
       .then((data) => {
-      let cuatro= data.results.filter((peli,idx)=>idx<4)
-      this.setState({ seriesHoy: cuatro})})
+        let cuatro = data.results.filter((peli, idx) => idx < 4);
+        this.setState({ peliculasNowPlaying: cuatro });
+      })
       .catch((err) => console.error(err));
 
-      fetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", options)
+    fetch(
+      "https://api.themoviedb.org/3/tv/airing_today?language=en-US&page=1",
+      options
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        let cuatro = data.results.filter((peli, idx) => idx < 4);
+        this.setState({ seriesHoy: cuatro });
+      })
+      .catch((err) => console.error(err));
+
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+      options
+    )
       .then((res) => res.json())
       .then((data) => {
         let cuatro = data.results.filter((peli, idx) => idx < 4);
@@ -52,14 +64,16 @@ class Home extends Component {
       })
       .catch((err) => console.error(err));
 
-    fetch("https://api.themoviedb.org/3/tv/popular?language=en-US&page=1", options)
+    fetch(
+      "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1",
+      options
+    )
       .then((res) => res.json())
       .then((data) => {
         let cuatro = data.results.filter((peli, idx) => idx < 4);
         this.setState({ seriesPopulares: cuatro });
       })
       .catch((err) => console.error(err));
-  
   }
 
   render() {
@@ -68,109 +82,73 @@ class Home extends Component {
         <Navbar />
 
         <div className="container">
-
           <h2>Now Playing</h2>
           <section className="cards all-movies">
             {this.state.peliculasNowPlaying.length === 0 ? (
               <h3>Cargando...</h3>
             ) : (
               this.state.peliculasNowPlaying.map((pelicula) => (
-                <article key={pelicula.id} className="single-card-movie">
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}
-                    alt={pelicula.title}
-                    className="card-img-top"
-                  />
-                  <div className="cardBody">
-                    <h5 className="card-title">{pelicula.title}</h5>
-                    <Link to={`/peliculas/${pelicula.id}`} className="btn btn-primary">
-                      Ver detalle
-                    </Link>
-                  </div>
-                </article>
+                <CardPelis key={pelicula.id} pelicula={pelicula} />
               ))
             )}
           </section>
-
           <div className="load-more">
-            <Link to="/peliculasNP" className="btn btn-secondary">Ver todas</Link>
+            <Link to="/peliculasNP" className="btn btn-secondary">
+              Ver todas
+            </Link>
           </div>
 
+         
           <h2>Series Airing Today</h2>
           <section className="cards all-movies">
             {this.state.seriesHoy.length === 0 ? (
               <h3>Cargando...</h3>
             ) : (
               this.state.seriesHoy.map((serie) => (
-                <article key={serie.id} className="single-card-movie">
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500${serie.poster_path}`}
-                    alt={serie.name}
-                    className="card-img-top"
-                  />
-                  <div className="cardBody">
-                    <h5 className="card-title">{serie.name}</h5>
-                    <Link to={`/series/${serie.id}`} className="btn btn-primary">
-                    Ver detalle
-                    </Link>
-                  </div>
-                </article>
+                <CardSeries key={serie.id} serie={serie} />
               ))
             )}
           </section>
           <div className="load-more">
-            <Link to="/seriesAT" className="btn btn-secondary">Ver todas</Link>
+            <Link to="/seriesAT" className="btn btn-secondary">
+              Ver todas
+            </Link>
           </div>
-          
-          <h2>Películas populares</h2>
+
+         
+          <h2>Películas Populares</h2>
           <section className="cards all-movies">
             {this.state.peliculasPopulares.length === 0 ? (
               <h3>Cargando...</h3>
             ) : (
               this.state.peliculasPopulares.map((pelicula) => (
-                <article key={pelicula.id} className="single-card-movie">
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}
-                    alt={pelicula.title}
-                    className="card-img-top"
-                  />
-                  <div className="cardBody">
-                    <h5>{pelicula.title}</h5>
-                    <Link to={`/peliculas/${pelicula.id}`}>Ver detalle</Link>
-                  </div>
-                </article>
+                <CardPelis key={pelicula.id} pelicula={pelicula} />
               ))
             )}
           </section>
           <div className="load-more">
-            <Link to="/peliculas" className="btn btn-secondary">Ver todas</Link>
+            <Link to="/peliculas" className="btn btn-secondary">
+              Ver todas
+            </Link>
           </div>
 
-          <h2>Series populares</h2>
+         
+          <h2>Series Populares</h2>
           <section className="cards all-movies">
             {this.state.seriesPopulares.length === 0 ? (
               <h3>Cargando...</h3>
             ) : (
               this.state.seriesPopulares.map((serie) => (
-                <article key={serie.id} className="single-card-movie">
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500${serie.poster_path}`}
-                    alt={serie.name}
-                    className="card-img-top"
-                  />
-                  <div className="cardBody">
-                    <h5>{serie.name}</h5>
-                    <Link to={`/series/${serie.id}`}>Ver detalle</Link>
-                  </div>
-                </article>
+                <CardSeries key={serie.id} serie={serie} />
               ))
             )}
           </section>
           <div className="load-more">
-            <Link to="/series" className="btn btn-secondary">Ver todas</Link>
+            <Link to="/series" className="btn btn-secondary">
+              Ver todas
+            </Link>
           </div>
         </div>
-
 
         <Footer />
       </React.Fragment>
